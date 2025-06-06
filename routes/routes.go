3ea3537208dc42e7cca6f47da3fbb8e4c979ec5e
pativatkongsawat <pativat.k@kkumail.com>
2025-06-06 15:37:54 +0700

    [EDIT] ROUTES API

--- routes/routes.go
+++ routes/routes.go
@@ -1,5 +1,23 @@
 package routes
 
-func InitRoutes() {
+import (
+	controllercategory "go_gin/controller/ControllerCategory"
+	controllerproduct "go_gin/controller/ControllerProduct"
+	controlleruser "go_gin/controller/ControllerUser"
 
+	"github.com/gin-gonic/gin"
+)
+
+func InitRoutes(r *gin.Engine) {
+
+	r.GET("/product/all", controllerproduct.GetAllProduct)
+	r.GET("/product/all/filter", controllerproduct.GetProduct)
+	r.POST("/product/insert", controllerproduct.InsertProduct)
+
+	r.GET("/user/all", controlleruser.GetAllUser)
+	r.GET("/user/all/filter", controlleruser.GetUser)
+	r.POST("/user/insert", controlleruser.InsertUser)
+
+	r.GET("/category/all", controllercategory.GetAllCategory)
+	r.POST("/category/insert", controllercategory.InsertCategory)
 }
