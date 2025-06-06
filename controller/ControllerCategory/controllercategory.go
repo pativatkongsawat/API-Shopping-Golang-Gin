eb0365179e3d2bd8233cf15071c7b66059c1da51
pativatkongsawat <pativat.k@kkumail.com>
2025-06-06 13:13:21 +0700

    [ADD] INSERT CATEGORY DATA

--- controller/ControllerCategory/controllercategory.go
+++ controller/ControllerCategory/controllercategory.go
@@ -15,6 +15,35 @@ func InsertCategory(ctx *gin.Context) {
 
 	categorydata := []category.Category{}
 
+	if err := ctx.ShouldBindJSON(&categorydata); err != nil {
+
+		ctx.JSON(http.StatusBadRequest, utils.ResponseMessage{
+
+			Status:  400,
+			Message: "CATEGORY BAD REQUEST",
+			Result:  err.Error(),
+		})
+
+	}
+
+	category, err := categoryModelHelper.InsertCategory(categorydata)
+
+	if err != nil {
+
+		ctx.JSON(http.StatusInternalServerError, utils.ResponseMessage{
+			Status:  500,
+			Message: "ERROR INSERT CATEGORY",
+			Result:  err.Error(),
+		})
+
+	}
+
+	ctx.JSON(http.StatusOK, utils.ResponseMessage{
+		Status:  200,
+		Message: "INSERT CATEGORYSUCSSFULY",
+		Result:  category,
+	})
+
 }
 
 func GetAllCategory(ctx *gin.Context) {
--- models/category/categoryHelper.go
+++ models/category/categoryHelper.go
@@ -1,21 +1,38 @@
 package category
 
-import "gorm.io/gorm"
+import (
+	"gorm.io/gorm"
+)
 
 type CategoryModelHelper struct {
 	DB *gorm.DB
 }
 
 func (u *CategoryModelHelper) InsertCategory(data []Category) ([]Category, error) {
+	var categorydata []Category
 
-	return nil, nil
+	for _, d := range data {
+		category := Category{
+			Name: d.Name,
+		}
+
+		categorydata = append(categorydata, category)
+	}
+
+	if err := u.DB.Create(&categorydata).Error; err != nil {
+		return nil, err
+	}
+
+	return categorydata, nil
 }
 
 func (u *CategoryModelHelper) GetAllCategory() ([]Category, error) {
 
 	categorys := []Category{}
 
-	if err := u.DB.Find(&categorys).Error; err != nil {
+	tx := u.DB.Begin()
+
+	if err := tx.Find(&categorys).Error; err != nil {
 
 		return nil, err
 	}
