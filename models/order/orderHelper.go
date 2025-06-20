08b5807ff301bccbf6ed2cb8b877fc355e5312e0
pativatkongsawat <pativat.k@kkumail.com>
2025-06-20 16:04:03 +0700

    [ADD] Create Order

---
+++ models/order/orderHelper.go
@@ -0,0 +1,46 @@
+package order
+
+import (
+	"gorm.io/gorm"
+)
+
+type OrderModelHelper struct {
+	DB *gorm.DB
+}
+
+func (u *OrderModelHelper) CreateOrder(data *Order) (*Order, error) {
+
+	tx := u.DB.Begin()
+	if err := tx.Debug().Create(&data).Error; err != nil {
+
+		tx.Rollback()
+		return nil, err
+
+	}
+	tx.Commit()
+
+	return data, nil
+}
+
+func (u *OrderModelHelper) CreateOrderHasProduct(orderId int, data []RequestProducts) (*[]OrderHasProduct, error) {
+
+	orderdetail := []OrderHasProduct{}
+	tx := u.DB.Begin()
+
+	for _, p := range data {
+
+		newdata := OrderHasProduct{
+			OrderId:           orderId,
+			ProductId:         p.Id,
+			OrderProductTotal: p.Quantity,
+		}
+		orderdetail := append(orderdetail, newdata)
+
+		if err := tx.Debug().Table("order_has_products").Create(&orderdetail).Error; err != nil {
+			tx.Rollback()
+			return nil, err
+		}
+	}
+
+	return &orderdetail, nil
+}
